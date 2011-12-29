2dbf0890567847e1123a5ea5ea006ad001d1231d
Mike Grafton & Wei Lu <pair+mike+wei@pivotallabs.com>
2011-12-29 11:54:38 +0800

    basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate
+
+  USER_ID = "pivotal"
+  PASSWORD = "toddy8apple"
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == USER_ID && password == PASSWORD
+    end
+  end
+
 end
---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe HomeController do
+  describe "#index" do
+
+    context "when the basic auth credentials are supplied" do
+
+      before do
+        request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(
+            ApplicationController::USER_ID,
+            ApplicationController::PASSWORD
+        )
+      end
+
+      it "is successful" do
+        get :index
+        response.should be_success
+      end
+    end
+
+    context "when the basic auth credentials are not supplied" do
+      it "is not successful" do
+        get :index
+        response.code.should == "401"
+      end
+    end
+  end
+end
